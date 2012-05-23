bf7bede66e5b2aaaaa298e33efe23c70432b640e
Juho Vepsalainen <bebraw@gmail.com>
2012-05-23 13:12:20 +0300

    Fetch some basic info from given url

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -5,6 +5,37 @@ var github = new GitHubApi({version: '3.0.0'});
 main();
 
 function main() {
-    // TODO
+    fetchInfo('https://github.com/bebraw/colorjs',
+        function(d) {console.log(d);},
+        function(d) {console.log(d);}
+    );
 }
+
+function fetchInfo(url, errCb, okCb) {
+    var parts = url.split('/').filter(id).slice(-2);
+    github.repos.get({
+        user: parts[0],
+        repo: parts[1]
+    }, function(err, d) {
+        if(err) errCb(err);
+        else ok(d);
+    });
+
+    function ok(d) {
+        okCb({
+            name: d.name,
+            url: d.html_url,
+            homepage: d.homepage,
+            description: d.description,
+            versions: versions(d)
+        });
+    }
+
+    function versions(d) {
+        // TODO
+        return [];
+    }
+}
+
+function id(a) {return a;}
 
