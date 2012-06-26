d49bcca5a7502427f4fe2ad0d17dc9d16857fa48
Juho Vepsalainen <bebraw@gmail.com>
2012-06-26 15:14:32 +0300

    Refine test stub

--- tests/tests.js
+++ tests/tests.js
@@ -14,11 +14,39 @@ function main() {
     client.api(URL + '?apikey=' + APIKEY, function(err, api) {
         if(err) return console.log(err);
 
-        api.libraries.count(function(err, d) {
+        createTag(api, function(err) {
             if(err) return console.log(err);
 
-            console.log(d);
+            api.libraries.get({name: 'test'}, function(err, d) {
+                if(err) return console.log(err);
+console.log(d[0]);
+                if(d.length) deleteLibrary(api, d[0]._id, createLibrary);
+                else createLibrary(api);
+            });
         });
+    });
+}
+
+function createTag(api, cb) {
+    api.tags.create({name: 'demoTag'}, cb);
+}
+
+function deleteLibrary(api, id, cb) {
+    api.libraries['delete']({id: id}, function(err, d) {
+        if(err) console.log(err);
+        else cb(api);
+    });
+}
+
+function createLibrary(api) {
+    api.libraries.create({
+        name: 'test',
+        repository: 'http://www.demo.com'
+        //tags: ['demoTag']
+    }, function(err, d) {
+        if(err) return console.log(err);
+
+        console.log(d);
     });
 }
 
