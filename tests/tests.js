c3709ac7037d140b2b4c542f22178569a6c9434a
Juho Vepsäläinen <bebraw@gmail.com>
2012-08-20 14:03:09 +0300

    Test update

--- tests/tests.js
+++ tests/tests.js
@@ -18,18 +18,30 @@ function main() {
         createTag(api, function(err) {
             if(err) return console.log(err);
 
-            api.libraries.get({name: 'test'}, function(err, d) {
+            getLibrary(api, {name: 'test'}, function(err, d) {
                 if(err) return console.log(err);
 
-                if(d.length) deleteLibrary(api, d[0]._id, createLibrary);
-                else createLibrary(api);
+                if(d.length) deleteLibrary(api, d[0]._id, test);
+                else test(api);
             });
         });
     });
 }
 
+function test(api) {
+    createLibrary(api, updateLibrary);
+}
+
 function createTag(api, cb) {
     api.tags.create({name: 'demoTag21'}, cb);
+}
+
+function getLibrary(api, o, cb) {
+    api.libraries.get(o, function(err, d) {
+        if(err) return console.log(err);
+
+        cb(err, d);
+    });
 }
 
 function deleteLibrary(api, id, cb) {
@@ -39,7 +51,7 @@ function deleteLibrary(api, id, cb) {
     });
 }
 
-function createLibrary(api) {
+function createLibrary(api, cb) {
     // since the tag does not exist, does not set it to anything
     // probably ok behavior for now
     api.libraries.create({
@@ -49,7 +61,18 @@ function createLibrary(api) {
     }, function(err, d) {
         if(err) return console.log(err);
 
-        console.log(d);
+        console.log('created library', d);
+
+        cb(api, d);
     });
 }
 
+function updateLibrary(api, d) {
+    d.name = 'updated test';
+
+    api.libraries.update(d, function(err, d) {
+        if(err) return console.log(err);
+
+        console.log('updated library', d);
+    });
+}
