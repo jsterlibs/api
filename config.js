b9266f95fc7abc96e9d2cb36f03aa3787e17c2c0
Juho Vepsäläinen <bebraw@gmail.com>
2012-10-27 20:11:07 +0300

    Fix typo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var env = process.env;
 
 exports.APIKEY = env.APIKEY || 'dummy';
-exports.READONLY_APIKEY = env.READONLYAPIKEY || 'dummy2';
+exports.READONLY_APIKEY = env.READONLY_APIKEY || 'dummy2';
 exports.MONGO_URL = env.NODE_ENV == 'production'?
     env.MONGOHQ_URL: 'mongodb://localhost/jswiki';
 exports.PORT = env.PORT || 8000;
